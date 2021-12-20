1d36c7e48f3ab76a47df98f23eb99b499fa00c0f
Matthew Manion <mmanion10@gmail.com>
2021-12-20 16:42:52 -0500

    Added CardListView

---
+++ StudyCards/Views/Cards/CardListView.swift
@@ -0,0 +1,50 @@
+//
+//  CardListView.swift
+//  StudyCards
+//
+//  Created by Matthew Manion on 12/20/21.
+//
+
+import SwiftUI
+
+struct CardListView: View {
+  var cards: [Card] = []
+  @State var showForm = false
+
+  var body: some View {
+    NavigationView {
+      VStack {
+        Spacer()
+        VStack {
+          GeometryReader { geometry in
+            ScrollView(.horizontal) {
+              HStack(spacing: 10) {
+                ForEach(cards, id: \.id) { card in
+                  CardView(card: card)
+                    .padding([.leading, .trailing])
+                }
+              }.frame(height: geometry.size.height)
+            }
+          }
+        }
+        Spacer()
+      }
+      .sheet(isPresented: $showForm) {
+        NewCardForm()
+      }
+      .navigationBarTitle("🔥 Fire Cards")
+        // swiftlint:disable multiple_closures_with_trailing_closure
+        .navigationBarItems(trailing: Button(action: { showForm.toggle() }) {
+          Image(systemName: "plus")
+            .font(.title)
+        })
+    }
+    .navigationViewStyle(StackNavigationViewStyle())
+  }
+}
+
+struct CardListView_Previews: PreviewProvider {
+  static var previews: some View {
+    CardListView(cards: testData)
+  }
+}

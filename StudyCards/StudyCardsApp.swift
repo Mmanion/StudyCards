a1487f87bac07723fe366b6abda7a55eead51a4f
Matthew Manion <mmanion10@gmail.com>
2021-12-20 16:22:55 -0500

    InitalSetup

--- StudyCards/StudyCardsApp.swift
+++ StudyCards/StudyCardsApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct StudyCardsApp: App {
+    init() {
+        FirebaseApp.configure()
+ //       Auth.auth().signInAnonymously()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()

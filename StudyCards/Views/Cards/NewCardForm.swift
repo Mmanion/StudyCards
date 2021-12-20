17d9e16c91761159c8cd4824e69ca97273232c0e
Matthew Manion <mmanion10@gmail.com>
2021-12-20 16:44:30 -0500

    Added NewCardForm

---
+++ StudyCards/Views/Cards/NewCardForm.swift
@@ -0,0 +1,43 @@
+//
+//  NewCardForm.swift
+//  StudyCards
+//
+//  Created by Matthew Manion on 12/20/21.
+//
+
+import SwiftUI
+
+struct NewCardForm: View {
+  @State var question: String = ""
+  @State var answer: String = ""
+  @Environment(\.presentationMode) var presentationMode
+
+  var body: some View {
+    VStack(alignment: .center, spacing: 30) {
+      VStack(alignment: .leading, spacing: 10) {
+        Text("Question")
+          .foregroundColor(.gray)
+        TextField("Enter the question", text: $question)
+          .textFieldStyle(RoundedBorderTextFieldStyle())
+      }
+      VStack(alignment: .leading, spacing: 10) {
+        Text("Answer")
+          .foregroundColor(.gray)
+        TextField("Enter the answer", text: $answer)
+          .textFieldStyle(RoundedBorderTextFieldStyle())
+      }
+      Button(action: {}) {
+        Text("Add New Card")
+          .foregroundColor(.blue)
+      }
+      Spacer()
+    }
+    .padding(EdgeInsets(top: 80, leading: 40, bottom: 0, trailing: 40))
+  }
+}
+
+struct NewCardForm_Previews: PreviewProvider {
+  static var previews: some View {
+    NewCardForm()
+  }
+}

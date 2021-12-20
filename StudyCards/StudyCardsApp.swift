d6a888d0262a43e069034ba5e142d096ca5bb751
Matthew Manion <mmanion10@gmail.com>
2021-12-20 14:59:54 -0500

    Initial Commit

---
+++ StudyCards/StudyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCardsApp.swift
+//  StudyCards
+//
+//  Created by Matthew Manion on 12/20/21.
+//
+
+import SwiftUI
+
+@main
+struct StudyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
